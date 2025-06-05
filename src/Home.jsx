4bfa4fbc6f532ebad55c24cfde0a789f63410975
Amal Sindhu Sasidharan <amal0109@gmail.com>
2025-06-05 11:13:39 +0530

    using fetch api to get dummy users api response

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    fakeiApi();
+  }, []);
+
+  const fakeiApi = () => {
+    fetch("https://fakestoreapi.com/users")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
   return <div>Home component</div>;
 };
 
